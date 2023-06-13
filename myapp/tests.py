a14fcd433fefb5468519d1fb49bba10280a630a9
janeffer945 <janeffernjeri945@gmail.com>
2023-06-13 19:18:15 +0300

    added test for company creation

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from myapp.models import Companies
 
-# Create your tests here.
+class AnimalTestCase(TestCase):
+    def setUp(self):
+       Companies.objects.create(name="test limited", email="test@gmail.com")
+
+    def test_create_company(self):
+        "user should be able to create a company"
+        company =Companies.objects.get(email="test@gmail.com")
+        self.assertEqual(company.name,'test limited')
