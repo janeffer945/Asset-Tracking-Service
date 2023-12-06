1abb8cceae62d3a944d1fffb41896181cf6c64ff
janeffer945 <janeffernjeri945@gmail.com>
2023-12-06 11:08:49 +0300

    login and signup path

--- myapp/urls.py
+++ myapp/urls.py
@@ -18,4 +18,5 @@ urlpatterns = [
     path('', include(router.urls)),
     
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('login', view='login', name= 'login')
 ]
