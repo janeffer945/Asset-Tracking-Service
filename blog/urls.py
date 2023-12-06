65bfd7177abdb9d8752f5a203dbfeca6386751c0
janeffer945 <janeffernjeri945@gmail.com>
2023-12-06 09:22:33 +0300

    update: project installed  requirements

--- blog/urls.py
+++ blog/urls.py
@@ -16,8 +16,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include 
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.schemas import get_schema_view
+# from django.views.generic import TemplateView...
+# from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Docs",
+      default_version='v1',
+      description="Company Name Register",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@xyz.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
-    path('auth/', obtain_auth_token)
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('admin/', admin.site.urls),
+   path('', include('myapp.urls')),
+   path('auth/', obtain_auth_token)
 ]
--- myapp/urls.py
+++ myapp/urls.py
@@ -3,10 +3,6 @@ from django.urls import include, path
 from rest_framework import routers
 from . import views
 # from django.conf.urls import path
-from rest_framework_swagger.views import get_swagger_view
-
-
-schema_view = get_swagger_view(title='Corporate App Track APIs')
 
 
 router = routers.DefaultRouter()
@@ -15,9 +11,11 @@ router.register(r'Employees',  views.EmployeesViewSet)
 router.register(r'Devices', views.DevicesViewSet )
 router.register(r'Device_log', views.Device_logViewSet )
 
-schema_view = get_swagger_view(title='Corporate App Track APIs')
+
+
 urlpatterns = [
    
     path('', include(router.urls)),
+    
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
